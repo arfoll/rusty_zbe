59ea382019af348df096696276bc8ef88a92ea81
Brendan Le Foll <brendan@fridu.org>
2022-06-08 17:05:19 +0200

    working example that writes and reads can frame

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,45 @@
-extern crate clap;
+//use futures_timer::Delay;
+//use std::time::Duration;
+use futures_util::stream::StreamExt;
+use tokio::time::{sleep, Duration};
+use tokio_socketcan::{CANFrame, CANSocket, Error};
 
-use clap::{Arg, Command};
+async fn keepalive(canif:String) {
+    let cansock_tx = CANSocket::open(&canif).unwrap();
+    let nm3frame = CANFrame::new(0x1, &[0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08], false, false).unwrap();
 
-fn main() {
-    // basic app information
-    let app = Command::new("hello-clap")
-        .version("1.0")
-        .about("Says hello")
-        .author("Michael Snoyman");
+    loop {
+        println!("Writing on vcan0");
+        // why do I need to unwrap the result and '?' doesn't work in a fn?
+        let nm3 = cansock_tx.write_frame(nm3frame).unwrap();
+        nm3.await;
+        sleep(Duration::from_millis(1000)).await;
+        println!("Waiting 3 seconds");
+        //Delay::new(Duration::from_secs(3)).await;
+    }
+}
 
-    // Define the name command line option
-    let name_option = Arg::new("name")
-        .long("name") // allow --name
-        .takes_value(true)
-        .help("Who to say hello to")
-        .required(true);
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let canif = "vcan0";
 
-    // now add in the argument we want to parse
-    let app = app.arg(name_option);
+    tokio::spawn(async move {
+        // is good?
+        keepalive(canif.to_string()).await;
+    });
 
-    // extract the matches
-    let matches = app.get_matches();
+    // doesn't support copy so I have to do this twice? Berk
+    let mut cansock_rx = CANSocket::open(&canif).unwrap();
 
-    // Extract the actual name
-    let name = matches.value_of("name")
-        .expect("This can't be None, we said it was required");
-
-    println!("Hello, {}!", name);
+// LOOP not needed!
+// Use BCM to filter messages?
+    loop {
+//        let frame = CANFrame::new(0xFF, &[0], false, false).unwrap();
+//        cansock.write_frame(frame)?.await?;
+//        sleep(Duration::from_millis(1000)).await;
+        while let Some(next) = cansock_rx.next().await {
+            println!("{:#?}", next);
+        }
+        println!("endofloop!!!");
+    }
 }
