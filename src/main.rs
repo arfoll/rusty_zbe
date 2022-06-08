58b9705e9317164eab864253791a5df0561c5dc0
Brendan Le Foll <brendan@fridu.org>
2022-06-08 23:54:16 +0200

    add working extremely basic uinput support

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use clap::{Arg, Command};
 use futures_util::stream::StreamExt;
 use tokio::time::{sleep, Duration};
 use tokio_socketcan::{CANFilter, CANFrame, CANSocket, Error};
+use uinput_tokio::event::keyboard;
 
 // CAN ID in extended format is 29bit max in extended format
 const IDRIVE_CAN_DATA_ID: u32 = 0x25B;
 const IUK_CAN_NM3_MSG_ID: u32 = 0x010;
 const IUK_CAN_NM3_MSG_PAYLOAD: &'static [u8] =  &[0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08];
+const IUK_CAN_NM3_TIMEOUT: u64 = 1400;
 // /usr/include/linux/can.h:#define CAN_SFF_MASK 0x000007FFU /* standard frame format (SFF) */
 const CAN_SFF_MASK: u32 = 0x000007FF;
 const CAN_IF_ARG: &str = "canif";
@@ -14,15 +16,15 @@ const CAN_IF_DEFAULT: &str = "can0";
 
 async fn keepalive(canif:String) {
     let cansock_tx = CANSocket::open(&canif).unwrap();
-    let nm3frame = CANFrame::new(IDRIVE_CAN_DATA_ID, IUK_CAN_NM3_MSG_PAYLOAD, false, false).unwrap();
+    let nm3frame = CANFrame::new(IUK_CAN_NM3_MSG_ID, IUK_CAN_NM3_MSG_PAYLOAD, false, false).unwrap();
 
     loop {
         println!("Writing on vcan0");
         // why do I need to unwrap the result and '?' doesn't work in a fn?
         let nm3 = cansock_tx.write_frame(nm3frame).unwrap();
         nm3.await;
-        sleep(Duration::from_millis(1000)).await;
-        println!("Waiting 3 seconds");
+        println!("Waiting 1 seconds");
+        sleep(Duration::from_millis(IUK_CAN_NM3_TIMEOUT)).await;
     }
 }
 
@@ -52,6 +54,17 @@ async fn main() -> Result<(), Error> {
         keepalive(caniflocal).await;
     });
 
+    let mut device = uinput_tokio::open("/dev/uinput")
+        .unwrap()
+        .name("test")
+        .unwrap()
+        .event(uinput_tokio::event::Keyboard::All)
+        .unwrap()
+        .create()
+        .await
+        .unwrap();
+
+
     // doesn't support copy so I have to do this twice? Berk
     let mut cansock_rx = CANSocket::open(&canif).unwrap();
     println!("Going to use, {}!", canif);
@@ -61,10 +74,14 @@ async fn main() -> Result<(), Error> {
 
     cansock_rx.set_filter(&can_filters);
 
-// why is LOOP not needed?
-// Use BCM to filter messages?
+    // why is LOOP not needed?
+    // Use BCM to filter messages?
     while let Some(next) = cansock_rx.next().await {
         println!("{:#?}", next);
+
+        device.click(&keyboard::Key::B).await.unwrap();
+        // Not sure why you need to but if you dont sync then udev will wait for 4 chars to arrive and then send
+        device.synchronize().await.unwrap();
     }
 
     Ok(())
